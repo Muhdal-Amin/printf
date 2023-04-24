31cf1d76581a32af71a8615db6a524619725e7ca
Muhdal-Amin <adalemo2000@gmail.com>
2023-04-24 16:14:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains my task files for the ALX Holberton school Printf project.
---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * _printf - custom implementation of printf function
+ * @format: format string
+ *
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+			case 'c':
+				count += putchar(va_arg(args, int));
+				break;
+			case 's':
+				count += printf("%s", va_arg(args, char *));
+				break;
+			case '%':
+				count += putchar('%');
+				break;
+			default:
+				putchar('%');
+				putchar(*format);
+				count += 2;
+				break;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+
+	va_end(args);
+
+	return (count);
+}
