9e2fe3de79924937bed37f5c3b3294b2b203c89f
Muhdal-Amin <adalemo2000@gmail.com>
2023-04-25 10:12:44 -0700

    Update - File

---
+++ _printf.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: character string with directives for formatting output
+ * Return: number of characters printed (excluding null byte)
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+			case 'c':
+				count += putchar(va_arg(args, int));
+				break;
+			case 's':
+				count += printf("%s", va_arg(args, char *));
+				break;
+			case '%':
+				count += putchar('%');
+				break;
+			case 'd':
+			case 'i':
+				count += printf("%d", va_arg(args, int));
+				break;
+			default:
+				putchar('%');
+				putchar(*format);
+				count += 2;
+				break;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
